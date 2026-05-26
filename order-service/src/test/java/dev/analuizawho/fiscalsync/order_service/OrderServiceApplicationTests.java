6243ef68a938f3bf9ab56d3f009472320f6847ea
analuizawho <analuiza319@hotmail.com>
2026-05-25 22:06:14 -0300

    first commit

---
+++ order-service/src/test/java/dev/analuizawho/fiscalsync/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.analuizawho.fiscalsync.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
